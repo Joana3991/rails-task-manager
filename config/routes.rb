668626b0b2db430d0cf84b3eb17c426d3eb035df
Joana Areas <jo_areas@hotmail.com>
2022-10-03 19:56:10 -0300

    root edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'tasks#index'
   resources :tasks
   # get '/tasks', to: 'tasks#index'
   # get '/tasks/new', to: 'tasks#new'
